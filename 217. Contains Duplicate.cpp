d59a3ab7f9dedc724d7a34b7ce758358d6dd1cb6
Joseph Tschopp <113856576+jo-jil@users.noreply.github.com>
2024-01-09 00:02:50 -0500

    Create 217. Contains Duplicate.cpp

---
+++ 217. Contains Duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> numSet;
+        for(int i = 0; i < nums.size(); i++){
+            if (numSet.find(nums[i]) != numSet.end()){
+                return true;
+            }
+            numSet.insert(nums[i]);
+        }
+        return false;
+    }
+};
